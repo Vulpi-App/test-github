a6aeeaf7fb646a81068be259338106344a542e73
Manon Boiteau <manonboiteau@MacBook-Pro-de-Manon.local>
2021-05-23 11:26:53 +0200

    delete linear

--- containers/HomeScreen.js
+++ containers/HomeScreen.js
@@ -4,14 +4,13 @@ import { Button, Text, View } from "react-native";
 
 export default function HomeScreen() {
   const navigation = useNavigation();
+  const test = "Hello test";
   return (
     <View>
       <Text>Ajout d'un texte !</Text>
       <Text>Ca fait faire des erreurs !</Text>
-
-      <Text>Ajout d'un second texte !</Text>
-
       <Text>Welcome home!</Text>
+      <Text>Ajout d'un autre text</Text>
       <Button
         title="Go to Profile"
         onPress={() => {
