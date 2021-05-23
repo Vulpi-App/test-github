3196ccb3b7dec24a56d0555a56feba1243da2013
Valentin Le Belleguy <lebelleguy.v@gmail.com>
2021-05-23 10:57:04 +0200

    add linear gradient

--- containers/ProfileScreen.js
+++ containers/ProfileScreen.js
@@ -2,12 +2,20 @@ import React from "react";
 import { useRoute } from "@react-navigation/core";
 import { Text, View } from "react-native";
 
+import LinearGradient from "react-native-linear-gradient";
+
 export default function ProfileScreen() {
   // Hello la team Vulpi
   const { params } = useRoute();
   return (
     <View>
       <Text>user id : {params.userId}</Text>
+      <LinearGradient
+        colors={["#4c669f", "#3b5998", "#192f6a"]}
+        style={styles.linearGradient}
+      >
+        <Text style={styles.buttonText}>Sign in with Facebook</Text>
+      </LinearGradient>
     </View>
   );
 }
