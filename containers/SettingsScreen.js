8dbb5128da04324c9305da1040655bcf0032b33a
Pauline-Bouchereau <pauline-bouchereau@gmail.com>
2021-05-23 10:35:39 +0200

    ajout texte

--- containers/SettingsScreen.js
+++ containers/SettingsScreen.js
@@ -5,6 +5,7 @@ export default function SettingsScreen({ setToken }) {
   return (
     <View>
       <Text>Hello Settings</Text>
+      <Text>J'ai rajouté un texte !</Text>
 
       <Button
         title="Log Out"
